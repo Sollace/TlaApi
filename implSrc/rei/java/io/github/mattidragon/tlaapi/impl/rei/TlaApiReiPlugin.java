6a3ad98751db322a3b3c55afe54ac675420353ad
Sollace <sollacea@gmail.com>
2025-05-26 19:50:33 +0100

    Implement removeRecipes function for REI

--- implSrc/rei/java/io/github/mattidragon/tlaapi/impl/rei/TlaApiReiPlugin.java
+++ implSrc/rei/java/io/github/mattidragon/tlaapi/impl/rei/TlaApiReiPlugin.java
@@ -278,8 +278,7 @@ public class TlaApiReiPlugin implements REIClientPlugin, PluginContext {
 
     @Override
     public void removeRecipes(Predicate<TlaRecipe> predicate) {
-        // TODO Auto-generated method stub
-
+        recipeHidePredicate = recipeHidePredicate.and(predicate);
     }
 
     @Override
